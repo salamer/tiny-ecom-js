88023ee9a3dfea9d5aedb0cce19e3405bc79a703
salamer <salamer_gaga@163.com>
2023-09-15 00:33:43 +0800

    fix

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require('express');
 const leapcell = require('@leapcell/leapcell-js');
+// let engine = require('ejs-locals');
+
 const path = require('path');
 const app = express();
+// app.engine('ejs', engine);
+app.set('views', './views');
 app.set('view engine', 'ejs');
+
+const api = new leapcell.Leapcell({
+    apiKey: 'lpcl_3079002420.21e580a433ce0b1cae979ddfd8b33021',
+    endpoint: 'http://localhost:8080',
+});
+const table = api.repo('salamer/myblog').table('tbl1702010602858938368', 'name');
+
+
 app.get('/', async (request, response) => {
-    const api = new leapcell.Leapcell({
-        apiKey: 'lpcl_3079002420.21e580a433ce0b1cae979ddfd8b33021',
-        endpoint: 'http://localhost:8080',
-    });
-    const table = api.repo('salamer/myblog').table('tbl1702010602858938368', 'name');
-    const records = await table.records.findMany({});
+    const res = await table.records.findMany({});
     return response.render('index', {
-        subject: 'EJS template engine',
-        name: 'our template',
-        link: 'https://google.com'
+        products: res.records,
     });
 });
+
+app.get("/product/:id", async (request, response) => {
+
+    const res = await table.records.findById(request.params.id);
+    return response.render('product', {
+        product: res.record,
+    });
+});
+
 app.listen(8000, () => {
     console.log('App is listening on port 8000');
 });
